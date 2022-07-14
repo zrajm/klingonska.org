e16a3d0c49b84ac5f931fc406e5e2c51fee417a3
zrajm <zrajm@klingonska.org>
2022-07-14 12:23:30 +0200

    Removed obsolete polyfills on glossary page

--- site/glossary/page.js
+++ site/glossary/page.js
@@ -25,64 +25,6 @@
     /*****************************************************************************\
     *******************************************************************************
     \*****************************************************************************/
-    /*file: compatibility */
-
-    /*****************************************************************************\
-    **                                                                           **
-    ** ECMAScript 5 Compatibility Functions for Older Browsers                   **
-    **                                                                           **
-    \*****************************************************************************/
-    (function () {
-        // Add Object.create()
-        if (typeof Object.create !== 'function') {
-            Object.create = function (o) {
-                function F() {}
-                F.prototype = o;
-                return new F();
-            };
-        }
-        // Add Object.keys()
-        if (!Object.keys) {
-            Object.keys = (function () {
-                var hasOwnProperty = Object.prototype.hasOwnProperty,
-                    hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
-                    dontEnums = [
-                        'toString',
-                        'toLocaleString',
-                        'valueOf',
-                        'hasOwnProperty',
-                        'isPrototypeOf',
-                        'propertyIsEnumerable',
-                        'constructor'
-                    ],
-                    dontEnumsLength = dontEnums.length;
-                return function (obj) {
-                    var result = [], prop, i;
-                    if ((typeof obj !== 'object' && typeof obj !== 'function') || obj === null) {
-                        throw new TypeError('Object.keys called on non-object');
-                    }
-                    for (prop in obj) {
-                        if (obj.hasOwnProperty(prop)) {
-                            result.push(prop);
-                        }
-                    }
-                    if (hasDontEnumBug) {
-                        for (i = 0; i < dontEnumsLength; i += 1) {
-                            if (hasOwnProperty.call(obj, dontEnums[i])) {
-                                result.push(dontEnums[i]);
-                            }
-                        }
-                    }
-                    return result;
-                };
-            }());
-        }
-    }());
-
-
-    /*****************************************************************************\
-    *******************************************************************************
-    \*****************************************************************************/
     /*file: makeinputtext */
 
     // This keeps track of a text input field, and store whatever is written there
